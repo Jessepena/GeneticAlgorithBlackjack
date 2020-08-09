6bd98bbac8b93c55179d917db361fda26af0b608
Jesse <jessepena@hotmail.es>
2020-08-09 16:29:04 -0400

    Created class TestConditions

---
+++ GeneticAlgorithBlackjack/utils/TestConditions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GeneticAlgorithBlackjack.Utils
+{
+    public class TestConditions
+    {
+            public int NumDecks { get; set; } = 4;
+
+            public int NumHandsToPlay { get; set; } = 100000;
+
+            public int BetSize { get; set; } = 2;
+
+            public int BlackjackPayoffSize { get; set; } = 3;   // Pago en caso de tener blackjack, la mayoría de los casinos pagan 3:2
+
+    }  
+}
